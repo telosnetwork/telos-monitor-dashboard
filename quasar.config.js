d299c4be945f81c386cec22165dfdf8056b52295
Thomas Cuvillier <titiomm@gmail.com>
2022-07-28 23:15:08 +0200

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -14,14 +14,6 @@ const { configure } = require('quasar/wrappers');
 
 module.exports = configure(function (/* ctx */) {
   return {
-    eslint: {
-      // fix: true,
-      // include = [],
-      // exclude = [],
-      // rawOptions = {},
-      warnings: true,
-      errors: true
-    },
 
     // https://v2.quasar.dev/quasar-cli/prefetch-feature
     // preFetch: true,
